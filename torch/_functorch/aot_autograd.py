5f3a084499bf1ff4c50bae2317f7c4c780639220
voznesenskym <voznesenskym@gmail.com>
2023-11-08 13:33:03 -0800

    [dynamo] handle setting .data on a tensor
    cleanup
    
    fix tests
    
    ghstack-source-id: cc5b0d44de468a2146eb559a9b815dc2f9525dfe
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/113080
    
    fix tests
    
    fix tests
    
    fix tests
    
    [dynamo] Not working yet - .add_() w/ aot_autograd (missing synthetic_base_info)
    
    ghstack-source-id: cc5b0d44de468a2146eb559a9b815dc2f9525dfe
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/113081
    
    fix tests
    
    aot_autograd fixes
    
    fix tests
    
    Refactor
    
    Revert "[dynamo] Graph break on `setattr(Tensor, "data", Tensor)` (#113043)"
    
    This reverts commit ddfe5725342b0c0f707222879ca9dac305f97210.
    
    ghstack-source-id: cc5b0d44de468a2146eb559a9b815dc2f9525dfe
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/113205
    
    Refactor
    
    Refactor
    
    Refactor
    
    Refactor
    
    Refactor
    
    Refactor
    
    cache to shape env
    
    ghstack-source-id: cc5b0d44de468a2146eb559a9b815dc2f9525dfe
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/113295
    
    cache to shape env

--- torch/_functorch/aot_autograd.py
+++ torch/_functorch/aot_autograd.py
@@ -1127,7 +1127,7 @@ def run_functionalized_fw_and_collect_metadata(
                 mutates_metadata=mutates_metadata,
                 mutations_hidden_from_autograd=mutations_hidden_from_autograd,
                 mutates_storage_metadata=mutates_storage_metadata,
-                requires_grad=isinstance(f_arg, torch.Tensor) and f_arg.requires_grad
+                requires_grad=isinstance(f_arg, torch.Tensor) and f_arg.requires_grad,
             ))
 
         # If a function involves creating a tensor, and returning a view of it, such that its _base is the intermediate,
@@ -2730,6 +2730,7 @@ def aot_wrapper_dedupe(
     args_set = set()
     ok = True
 
+    # breakpoint()
     for i, a in enumerate(flat_args):
         if not isinstance(a, torch.Tensor):
             leaf_flat_args.append(a)
@@ -3137,7 +3138,20 @@ def create_runtime_wrapper(
                     if trace_joint:
                         assert isinstance(updated_inpt, TensorAlias)
                         updated_inpt = updated_inpt.alias
-                    original_inpt.set_(updated_inpt)
+                    try:
+                        original_inpt.set_(updated_inpt)
+                    except RuntimeError:
+                        # This happens when the sizes of the two tensors do not match, probably
+                        # because we saw .data
+                        # while we could call
+                        # original_inpt.data = updated_inpt
+                        # That's a little gross, let's use the "safe" pattern instead
+                        with torch.no_grad():
+                            version_counter = original_inpt._version
+                            original_inpt.set_(updated_inpt)
+                            if version_counter > 0:
+                                version_counter = version_counter - 1
+                            torch._C._autograd._unsafe_set_version_counter(original_inpt, version_counter)
                     continue
                 if meta.mutates_metadata and not meta.mutates_data:
                     if trace_joint:
@@ -4296,14 +4310,9 @@ def create_aot_dispatcher_function(
                     if all(isinstance(getattr(x, attr), FakeTensor) for attr in attrs):
                         assert all(getattr(x, attr).fake_mode is fake_mode for attr in attrs)
                         return x
-                # TODO: Ensure that this codepath is never exercised from
-                # Dynamo
-                if (
-                    idx < aot_config.num_params_buffers
-                    and config.static_weight_shapes
-                ):
-                    return fake_mode.from_tensor(x, static_shapes=True)
-                return fake_mode.from_tensor(x, static_shapes=False)
+                static_shapes = idx < aot_config.num_params_buffers and config.static_weight_shapes
+                out = fake_mode.from_tensor(x, static_shapes=static_shapes)
+                return out
 
             return [convert(idx, x) for idx, x in enumerate(flat_args)]
 
